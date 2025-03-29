ed14486e540920bcb6c33edcd2761853fe939422
Anusara97 <anusara777rcm@gmail.com>
2025-03-29 21:09:27 +0530

    Both files are Modified

--- Test.java
+++ Test.java
@@ -1,7 +1,14 @@
 public class Test {
+	int age;
+
+	public Test(int age) {
+		this.age = age;
+	}
+
 	public static void main(String[] args) {
 		String name = "Anusara";
 		String address = "Matara";
+
 	}
 
 	public static void Display(String name, String address, int age) {
