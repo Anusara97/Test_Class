af6c31a78e968b535ad85ad5ffc956c9d4c56394
Anusara97 <anusara777rcm@gmail.com>
2025-03-30 06:13:27 +0530

    Test.java & School.java is updated.

--- Test.java
+++ Test.java
@@ -2,9 +2,7 @@ public class Test {
 	public static void main(String[] args) {
 		String name = "Anusara";
 		String address = "Matara";
-	}
 
-	public static void Display(String name, String address, int age) {
-		
+		System.out.println("Name: " + name + "\nAddress: " + address);
 	}
 }
